dcffa5c69d8972bd31e7aa6eff9748a0893e9289
Caroline Rodrigues <caroline101910@hotmail.com>
2025-08-21 19:02:07 -0300

    Primeiro commit do projeto

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/pages/layout/Container';
+import Navbar from './components/pages/layout/Navbar';
+import Footer from './components/pages/layout/Footer';
+
+
+function App() {
+
+  return (
+
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+
+            <Route exact path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/newproject" element={ <NewProject />} />
+
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App;
